e0b65ae26fcaf66e4ac3cfdc531ec8b939f6402e
Tran Quang Ha <tranquangha2006@gmail.com>
2026-08-19 22:26:30 +0700

    add ghostty terminal config

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -270,7 +270,7 @@ hl.device({
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd("ghostty"))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
